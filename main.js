c1acee93e97350528b620d1fe190c0409f73d698
Oliver Szabo <oliverszbo@gmail.com>
2021-08-05 12:24:04 +0200

    edit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const _load = () => {
   const nav = document.getElementById("nav");
   const navOpen = document.getElementById("nav-open");
-  const x = document.getElementById("x");
+
   nav.addEventListener("click", () => {
     navOpen.classList.toggle("display");
   });
